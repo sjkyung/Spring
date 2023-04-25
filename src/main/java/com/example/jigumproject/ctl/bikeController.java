fdc5d49214ef18e17f400a438e5114230ef41deb
sjkyomg <sjkyomg@naver.com>
2023-04-25 22:50:41 +0900

    service openapi 연동

--- src/main/java/com/example/jigumproject/ctl/bikeController.java
+++ src/main/java/com/example/jigumproject/ctl/bikeController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,13 +21,24 @@ import java.util.List;
 @RequiredArgsConstructor
 public class bikeController {
 
-    BikeService bikeService;
+    private  final BikeService bikeService;
 
     @GetMapping(value = "ListAll")
     public List test() throws IOException {
 
         log.info("test");
-        bikeService.bikeList();
+        //bikeService.bikeList();
+
+        List test =new ArrayList();
+        return test;
+    }
+
+
+    @GetMapping(value = "bikeList/{start}/{end}")
+    public List bikeList(int start,int end) throws IOException {
+
+        log.info("start,end  : {},{} ",start, end);
+        bikeService.bikeList(start,end);
 
         List test =new ArrayList();
         return test;
--- src/main/java/com/example/jigumproject/service/BikeService.java
+++ src/main/java/com/example/jigumproject/service/BikeService.java
@@ -1,61 +1,48 @@
 package com.example.jigumproject.service;
 
 
+import com.example.jigumproject.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.net.URI;
+import java.util.Map;
+
 
 @Service
 @RequiredArgsConstructor
 public class BikeService {
 
 
-    public void bikeList() throws IOException {
 
-    // 1. URL을 만들기 위한 StringBuilder.
-    StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1262000/CountryCovid19SafetyServiceNew/getCountrySafetyNewsListNew"); /*URL*/
-    // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=인코딩된 서비스키"); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("returnType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*XML 또는 JSON*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
-        urlBuilder.append("&" + URLEncoder.encode("cond[country_nm::EQ]","UTF-8") + "=" + URLEncoder.encode("가나", "UTF-8")); /*한글 국가명*/
-        urlBuilder.append("&" + URLEncoder.encode("cond[country_iso_alp2::EQ]","UTF-8") + "=" + URLEncoder.encode("GH", "UTF-8")); /*ISO 2자리코드*/
-    // 3. URL 객체 생성.
-    URL url = new URL(urlBuilder.toString());
-    // 4. 요청하고자 하는 URL과 통신하기 위한 Connection 객체 생성.
-    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-    // 5. 통신을 위한 메소드 SET.
-        conn.setRequestMethod("GET");
-    // 6. 통신을 위한 Content-type SET.
-        conn.setRequestProperty("Content-type", "application/json");
-    // 7. 통신 응답 코드 확인.
-        System.out.println("Response code: " + conn.getResponseCode());
-    // 8. 전달받은 데이터를 BufferedReader 객체로 저장.
-    BufferedReader rd;
-        if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
-        rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-    } else {
-        rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
-    }
-    // 9. 저장된 데이터를 라인별로 읽어 StringBuilder 객체로 저장.
-    StringBuilder sb = new StringBuilder();
-    String line;
-        while ((line = rd.readLine()) != null) {
-        sb.append(line);
-    }
-    // 10. 객체 해제.
-        rd.close();
-        conn.disconnect();
-    // 11. 전달받은 데이터 확인.
-        System.out.println(sb.toString());
-    }
+        // http://localhost:9090/api/server/hello 로 요청해서 response를 받아오기
+        //http://openapi.seoul.go.kr:8088/464e43766a736a6b383768444b516b/json/bikeList/1/1000/ 따릉이 api
+        public Map bikeList(int start,int end){
+            // uri 주소 생성
+            URI uri = UriComponentsBuilder
+                    .fromUriString("http://openapi.seoul.go.kr:8088") //http://localhost에 호출
+                    .path("/464e43766a736a6b383768444b516b/json/bikeList")
+                    .path("/1/1000/")
+//                    .queryParam("name", "steve")  // query parameter가 필요한 경우 이와 같이 사용
+//                    .queryParam("age", 10)
+                    .encode()
+                    .build()
+                    .toUri();
+
+            System.out.println(uri.toString());
+
+            RestTemplate restTemplete = new RestTemplate();
+
+            ResponseEntity<Map> result = restTemplete.getForEntity(uri, Map.class);
+            // entity로 데이터를 가져오겠다(Get)~~
+            System.out.println(result.getStatusCode());
+            System.out.println(result.getBody());
+
+            return result.getBody();
+        }
 
 
 }

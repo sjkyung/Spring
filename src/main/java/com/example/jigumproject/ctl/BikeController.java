d10952de4fdde689907dd415dbb765f5dc711d22
sjkyomg <sjkyomg@naver.com>
2023-05-01 10:13:59 +0900

    cors 수정

--- src/main/java/com/example/jigumproject/ctl/BikeController.java
+++ src/main/java/com/example/jigumproject/ctl/BikeController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,6 +18,7 @@ import java.util.List;
 @Slf4j
 @Tag(name = "공공 api 연결 컨트롤러")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class BikeController {
 
     private  final BikeService bikeService;
